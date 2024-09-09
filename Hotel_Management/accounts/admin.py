d52eeee8cfc29d3665a8922735f34908baac8e84
54ntu <chaudharysantaram8@gmail.com>
2024-09-09 14:53:49 +0545

    customuser model is created and superuser is created

--- Hotel_Management/accounts/admin.py
+++ Hotel_Management/accounts/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import CustomUserModel
 
 # Register your models here.
+
+@admin.register(CustomUserModel)
+class CustomUserAdminModel(admin.ModelAdmin):
+    list_display = ('id','email','username','phone_no','fullname','city','roles_choices','is_active','is_superuser','is_staff')
--- Hotel_Management/accounts/models.py
+++ Hotel_Management/accounts/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from .managers  import CustomUserManager
+
 
 # Create your models here.
+class CustomUserModel(AbstractUser):
+    ROLE_CHOICES = (
+        ('admin', 'Admin'),
+        ('staff', 'Staff'),
+        ('guest', 'Guest'),
+    )
+    email = models.EmailField(unique=True,max_length=50)
+    phone_no = models.CharField(max_length=15)
+    fullname = models.CharField(max_length= 50)
+    city = models.CharField(max_length=50)
+    country = models.CharField(max_length=20)
+    roles_choices = models.CharField(choices=ROLE_CHOICES,max_length=50)
+
+
+
+    USERNAME_FIELD= "email"
+    REQUIRED_FIELDS =[]
+
+    objects = CustomUserManager()
